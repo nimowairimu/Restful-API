667ea271ac9146d7aeef68af37df1cf14eab563f
nimowairimu <nimowairimu25@gmail.com>
2021-04-05 17:20:29 +0300

    create a database.

--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,13 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from cloudinary.models import CloudinaryField
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
-# # Create your models here.
-# class Profile(models.Model):
-#     photo = CloudinaryField('images')
-#     Bio = models.CharField(max_length=30)
-#     user = models.OneToOneField(User,on_delete=models.CASCADE)
+Create your models here.
+class Profile(models.Model):
+    photo = CloudinaryField('images')
+    Bio = models.CharField(max_length=30)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     datecreated= models.DateField(auto_now_add=True )
 
 #     def __str__(self):
