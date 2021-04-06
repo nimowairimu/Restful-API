c435d6e4e9b15fc7a6f3df62972b084d33e37f4a
nimowairimu <nimowairimu25@gmail.com>
2021-04-06 22:18:21 +0300

    Add an edit profile template for users.

--- awwards/settings.py
+++ awwards/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ProjectsConfig',
     'bootstrap4',
     'cloudinary',
 ]
@@ -81,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'awwards',
-        'USER': 'nimo,
+        'USER': 'nimo',
     'PASSWORD':'kadesho62',
     }
 }
