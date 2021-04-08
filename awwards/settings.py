5d53f7566ce07d6b0792eba326896081182447f6
nimowairimu <nimowairimu25@gmail.com>
2021-04-08 16:47:45 +0300

    Deploy to heroku .

--- awwards/settings.py
+++ awwards/settings.py
@@ -180,7 +180,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),# Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # configuring the location for media
 MEDIA_URL = '/media/'
